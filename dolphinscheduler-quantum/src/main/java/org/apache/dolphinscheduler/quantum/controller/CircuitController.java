bc1cf9ae43414b2b0e182ebdccee215a8e9c692d
Khoi <khoinl@mz.co.kr>
2022-12-14 10:37:14 +0700

    add content type to export function

--- dolphinscheduler-quantum/src/main/java/org/apache/dolphinscheduler/quantum/controller/CircuitController.java
+++ dolphinscheduler-quantum/src/main/java/org/apache/dolphinscheduler/quantum/controller/CircuitController.java
@@ -37,6 +37,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -183,6 +184,7 @@ public class CircuitController extends BaseController {
                 .ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION,
                         "attachment; filename=\"" + name + ".json" + "\"")
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
                 .body(logBytes);
     }
 
