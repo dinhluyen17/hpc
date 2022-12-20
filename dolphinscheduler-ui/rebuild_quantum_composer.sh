7329e776189f5d3a7425a0a0550b341100bc5cee
dongnt <dongnt@mz.co.kr>
2022-12-20 14:43:23 +0700

    Update js when run build quirk

--- dolphinscheduler-ui/rebuild_quantum_composer.sh
+++ dolphinscheduler-ui/rebuild_quantum_composer.sh
@@ -3,4 +3,5 @@ npm run build
 cd ..
 cp ./quantum-composer/out/quirk.html ./dolphinscheduler-ui/public/quirk.html 
 cp ./quantum-composer/out/styles.css ./dolphinscheduler-ui/public/styles.css
+cp ./quantum-composer/out/script.js ./dolphinscheduler-ui/public/script.js
 cp -R ./quantum-composer/out/dist ./dolphinscheduler-ui/public
