33e717dac576c4afdf018f29854f6296eaa846a0
MZ01-PHUNV <phunv@mz.co.kr>
2022-12-22 15:27:15 +0700

    change tab style

--- dolphinscheduler-ui/src/views/projects/circuit/item/index.tsx
+++ dolphinscheduler-ui/src/views/projects/circuit/item/index.tsx
@@ -253,14 +253,14 @@ const circuitItem = defineComponent({
             }
           </NSpace>
           {/* Tab change area */}
-          <NSpace>
-            <NButton size='large' focusable={false} style={{ width: '200px' }} onClick={(e) => this.handleChangeTabCircuit(e)} class={`tab-button ${this.isCircuitTab ? 'active' : ''}`}>
+          <div>
+            <NButton size='large' focusable={false} style={{ width: '200px', borderRadius: 0 }} onClick={(e) => this.handleChangeTabCircuit(e)} class={`tab-button ${this.isCircuitTab ? 'active' : ''}`}>
               {t('circuit.detail.circuit')}
             </NButton>            
-            <NButton size='large' focusable={false} style={{ width: '200px' }} onClick={(e) => this.handleChangeTabSimulate(e)} class={`tab-button ${this.isCircuitTab ? '' : 'active'}`}>
+            <NButton size='large' focusable={false} style={{ width: '200px', marginLeft: '-1px', borderRadius: 0 }} onClick={(e) => this.handleChangeTabSimulate(e)} class={`tab-button ${this.isCircuitTab ? '' : 'active'}`}>
               {t('circuit.detail.simulate')}
             </NButton>
-          </NSpace>
+          </div>
           {/* Button area */}
           <NSpace>
             <input ref="importFileRef" type='file' onChange={handleImportFileJson} hidden/>
