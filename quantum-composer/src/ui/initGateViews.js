fcecbc2dc3d69f0f2d1b9947e39b920e8078c8c1
dinhluyen17 <luyendd@mz.co.kr>
2022-12-22 14:06:21 +0700

    define new gate symbols

--- quantum-composer/src/ui/initGateViews.js
+++ quantum-composer/src/ui/initGateViews.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
- import {Gates} from "../gates/AllGates.js";
+import { Gates } from "../gates/AllGates.js";
 /**
  * @param {!Revision} revision
  */
@@ -26,34 +26,113 @@ const initGateViews = () => {
   const advancedGatesGrid = document.getElementById('advanced-gates-grid');
   //list all unsupported gates and currently show all unsupported gates
   const unsupportedGates = [
-/*     "Postselect Off",
-    "Postselect On",
-    "Postselect X-Off",
-    "Postselect X-On",
-    "Postselect Y-Off",
-    "Postselect Y-On",
-    "Nothing Gate",
-    "Z Detect-Control-Reset",
-    "Y Detect-Control-Reset",
-    "X Detect-Control-Reset",
-    "Counting Gate",
-    "Down Counting Gate",
-    "Cycling Gradient Gate",
-    "Inverse Cycling Gradient Gate",
-    "Ne-Gate",
-    "Imaginary Gate",
-    "Anti-Imaginary Gate",
-    "Half Imaginary Gate",
-    "Half Anti-Imaginary Gate",
-    "X-Raising Gate (forward)",
-    "X-Raising Gate (backward)",
-    "Y-Raising Gate (forward)",
-    "Y-Raising Gate (backward)",
-    "Z-Raising Gate (forward)",
-    "Z-Raising Gate (backward)", */
-  ]; 
+    /*     "Postselect Off",
+        "Postselect On",
+        "Postselect X-Off",
+        "Postselect X-On",
+        "Postselect Y-Off",
+        "Postselect Y-On",
+        "Nothing Gate",
+        "Z Detect-Control-Reset",
+        "Y Detect-Control-Reset",
+        "X Detect-Control-Reset",
+        "Counting Gate",
+        "Down Counting Gate",
+        "Cycling Gradient Gate",
+        "Inverse Cycling Gradient Gate",
+        "Ne-Gate",
+        "Imaginary Gate",
+        "Anti-Imaginary Gate",
+        "Half Imaginary Gate",
+        "Half Anti-Imaginary Gate",
+        "X-Raising Gate (forward)",
+        "X-Raising Gate (backward)",
+        "Y-Raising Gate (forward)",
+        "Y-Raising Gate (backward)",
+        "Z-Raising Gate (forward)",
+        "Z-Raising Gate (backward)", */
+  ];
   //Gate group with 0 gate got supported by cirq or qiskit
   const unsupportedGroupGate = "" //supposed to be "Spinning" but right now just show every group gate on web
+
+  //new custom gate symbol
+  const newIcon = new Map([
+    ['Measure', '<small class="smaller">Measure</small>'],
+    ['◦', '<big class="bigger">◦</big>'],
+    ['Density', '<small class="smaller">Density</small>'],
+    ['Bloch', '<small>Bloch</small>'],
+    ['Chance', '<small class="smaller">Chance</small>'],
+    ['Amps', '<small>Amps</small>'],
+    ['Swap', '<small>Swap</small>'],
+    ['S^-1', 'S<sup>-1</sup>'],
+    ['Y^½', 'Y<sup>½</sup>'],
+    ['Y^-½', 'Y<sup>-½</sup>'],
+    ['X^½', 'X<sup>½</sup>'],
+    ['X^-½', 'X<sup>-½</sup>'],
+    ['T^-1', 'X<sup>-1</sup>'],
+    ['Y^¼', 'Y<sup>¼</sup>'],
+    ['Y^-¼', 'Y<sup>-¼</sup>'],
+    ['X^¼', 'X<sup>¼</sup>'],
+    ['X^-¼', 'X<sup>-¼</sup>'],
+    ['Z^t', 'Z<sup>t</sup>'],
+    ['Z^-t', 'Z<sup>-t</sup>'],
+    ['Y^t', 'Y<sup>t</sup>'],
+    ['Y^-t', 'Y<sup>-t</sup>'],
+    ['X^t', 'X<sup>t</sup>'],
+    ['X^-t', 'X<sup>-t</sup>'],
+    ['Z^ft', 'Z<sup>f(t)</sup>'],
+    ['Rzft', 'R<small>z(f(t))</small>'],
+    ['Y^ft', 'Y<sup>f(t)</sup>'],
+    ['Ryft', 'R<small>y(f(t))</small>'],
+    ['X^ft', 'X<sup>f(t)</sup>'],
+    ['Rxft', 'R<small>x(f(t))</small>'],
+    ['Z^A/2ⁿ', '<small>Z<small><sup>A/2<sup>n</sup></sup></small></small>'],
+    ['Z^-A/2ⁿ', '<small>Z<small><sup>-A/2<sup>n</sup></sup></small></small>'],
+    ['Y^A/2ⁿ', '<small>Y<small><sup>A/2<sup>n</sup></sup></small></small>'],
+    ['Y^-A/2ⁿ', '<small>Y<small><sup>-A/2<sup>n</sup></sup></small></small>'],
+    ['X^A/2ⁿ', '<small>X<small><sup>A/2<sup>n</sup></sup></small></small>'],
+    ['X^-A/2ⁿ', '<small>X<small><sup>-A/2<sup>n</sup></sup></small></small>'],
+    ['ZDetector', 'Z<small>D</small>'],
+    ['ZDetectControlReset', 'Z<small>DR</small>'],
+    ['YDetector', 'Y<small>D</small>'],
+    ['YDetectControlReset', 'Y<small>DR</small>'],
+    ['XDetector', 'X<small>D</small>'],
+    ['XDetectControlReset', 'X<small>DR</small>'],
+    ['zpar', 'z<br>[par]'],
+    ['ypar', 'y<br>[par]'],
+    ['xpar', 'x<br>[par]'],
+    ['Reverse', '<small class="smaller">Reverse</small>'],
+    ['Interleave', 'IL'],
+    ['Deinterleave', 'DIL'],
+    ['QFT^†', 'QFT<sup>†</sup>'],
+    ['Grad^½', '<small>Grad</small><sup>½</sup>'],
+    ['Grad^-½', '<small>Grad</small><sup>-½</sup>'],
+    ['Grad^t', '<small>Grad</small><sup>t</sup>'],
+    ['Grad^-t', '<small>Grad</small><sup>-t</sup>'],
+    ['input A', 'input<br>A'],
+    ['setA', 'A=#<br><small>defalut</small>'],
+    ['input B', 'input<br>B'],
+    ['setB', 'B=#<br><small>defalut</small>'],
+    ['input R', 'input<br>R'],
+    ['setR', 'R=#<br><small>defalut</small>'],
+    ['×A^-1', '×A<sup>-1</sup>'],
+    ['⊕A<B', '⊕<small>A&#60B</small>'],
+    ['⊕A>B', '⊕<small>A&#62B</small>'],
+    ['⊕A≤B', '⊕<small>A≤B</small>'],
+    ['⊕A≥B', '⊕<small>A≥B</small>'],
+    ['⊕A=B', '⊕<small>A=B</small>'],
+    ['⊕A≥B', '⊕<small>A≥B</small>'],
+    ['⊕A≠B', '⊕<small>A≠B</small>'],
+    ['+1\nmod R', '+1<br><small>mod R</small>'],
+    ['−1\nmod R', '-1<br><small>mod R</small>'],
+    ['+A\nmod R', '+A<br><small>mod R</small>'],
+    ['−A\nmod R', '-A<br><small>mod R</small>'],
+    ['×A\nmod R', '×A<br><small>mod R</small>'],
+    ['×A^-1\nmod R', '×A<sup>-1</sup><br><small>mod R</small>'],
+    ['×B^A\nmod R', '×B<sup>A</sup><br><small>mod R</small>'],
+    ['×B^-A\nmod R', '×B<sup>-A</sup><br><small>mod R</small>'],
+
+  ]);
 
   //show top gate as normal list
   Gates.TopToolboxGroups.forEach((group) => {
@@ -61,32 +140,13 @@ const initGateViews = () => {
       if (gate != undefined && !unsupportedGates.includes(gate.name)) {
         const gateView = document.createElement('span');
         gateView.setAttribute("class", "list-view tooltip-wrap")
-        const iconLink = `./icon/${gate.serializedId}.png`
-        let newIconLink = ''
-        if(iconLink.includes("Z^(A/2^n)")) {
-          newIconLink = iconLink.replace("Z^(A/2^n)", "Z^(A:2^n)")
-        } 
-        else if(iconLink.includes("Z^(-A/2^n)")) {
-          newIconLink = iconLink.replace("Z^(-A/2^n)", "Z^(-A:2^n)")
-        }
-        else if(iconLink.includes("Y^(A/2^n)")) {
-          newIconLink = iconLink.replace("Y^(A/2^n)", "Y^(A:2^n)")
-        }
-        else if(iconLink.includes("Y^(-A/2^n)")) {
-          newIconLink = iconLink.replace("Y^(-A/2^n)", "Y^(-A:2^n)")
-        }
-        else if(iconLink.includes("X^(A/2^n)")) {
-          newIconLink = iconLink.replace("X^(A/2^n)", "X^(A:2^n)")
-        }
-        else if(iconLink.includes("X^(-A/2^n)")) {
-          newIconLink = iconLink.replace("X^(-A/2^n)", "X^(-A:2^n)")
-        }
-        else {
-          newIconLink = iconLink
-        }
         gateView.innerHTML = `
-                    <img onmousedown="grabGate('${gate.serializedId}')" src=${newIconLink} style="width:36px; height:36px; margin: 5px 6px;"/>
-                    <span class="tooltip-content">${gate.name}</span>
+                      <div style='display: flex; align-items:center; justify-content:center; width:40px; height:40px; margin:0.3rem 0.25rem; border: 1px solid black'>
+                        <div style='text-align:center;'>
+                          ${(newIcon.get(gate.symbol) != null) ? newIcon.get(gate.symbol) : gate.symbol}
+                        </div>
+                      </div>
+                      <span class="tooltip-content">${gate.name}</span>
                 `;
         commonGates.appendChild(gateView)
       }
@@ -96,7 +156,7 @@ const initGateViews = () => {
   //show top gate as grid list
   let groupNum = 0
   Gates.TopToolboxGroups.forEach((group) => {
-    if(group.hint === unsupportedGroupGate) {return}
+    if (group.hint === unsupportedGroupGate) { return }
     let gateGroup = document.createElement('div'); gateGroup.setAttribute("class", "gate-group")
     let gateGroupName = document.createElement('h4'); gateGroupName.setAttribute("class", `group-gate-name`)
     gateGroupName.innerText = group.hint
@@ -114,24 +174,24 @@ const initGateViews = () => {
       if (gate != undefined && !unsupportedGates.includes(gate.name)) {
         const gateView = document.createElement('div');
         gateView.setAttribute("class", "grid-view")
-        const iconLink =`./icon/${gate.serializedId}.png`
+        const iconLink = `./icon/${gate.serializedId}.png`
         let newIconLink = ''
-        if(iconLink.includes("Z^(A/2^n)")) {
+        if (iconLink.includes("Z^(A/2^n)")) {
           newIconLink = iconLink.replace("Z^(A/2^n)", "Z^(A:2^n)")
-        } 
-        else if(iconLink.includes("Z^(-A/2^n)")) {
+        }
+        else if (iconLink.includes("Z^(-A/2^n)")) {
           newIconLink = iconLink.replace("Z^(-A/2^n)", "Z^(-A:2^n)")
         }
-        else if(iconLink.includes("Y^(A/2^n)")) {
+        else if (iconLink.includes("Y^(A/2^n)")) {
           newIconLink = iconLink.replace("Y^(A/2^n)", "Y^(A:2^n)")
         }
-        else if(iconLink.includes("Y^(-A/2^n)")) {
+        else if (iconLink.includes("Y^(-A/2^n)")) {
           newIconLink = iconLink.replace("Y^(-A/2^n)", "Y^(-A:2^n)")
         }
-        else if(iconLink.includes("X^(A/2^n)")) {
+        else if (iconLink.includes("X^(A/2^n)")) {
           newIconLink = iconLink.replace("X^(A/2^n)", "X^(A:2^n)")
         }
-        else if(iconLink.includes("X^(-A/2^n)")) {
+        else if (iconLink.includes("X^(-A/2^n)")) {
           newIconLink = iconLink.replace("X^(-A/2^n)", "X^(-A:2^n)")
         }
         else {
@@ -152,42 +212,18 @@ const initGateViews = () => {
   Gates.BottomToolboxGroups.forEach((group) => {
     group.gates.forEach(gate => {
       if (gate != undefined && !unsupportedGates.includes(gate.name)) {
-        const gateView = document.createElement('span');
-        gateView.setAttribute("class", "list-view tooltip-wrap")
-        const iconLink = `./icon/${gate.serializedId}.png`
-        let newIconLink = ''
-        if(iconLink.includes(">>3")) {
-          newIconLink = iconLink.replace(">>3", "2greater")
-        } 
-        else if(iconLink.includes("(/)")) {
-          newIconLink = iconLink.replace("(/)", "(:)")
-        } 
-        else if(iconLink.includes("|/⟩⟨/|")) {
-          newIconLink = iconLink.replace("|/⟩⟨/|", "|:⟩⟨:|")
-        }
-        else if(iconLink.includes("/A2")) {
-          newIconLink = iconLink.replace("/A2", ":A2")
-        }  
-        else if(iconLink.includes("^A>B")) {
-          newIconLink = iconLink.replace("^A>B", "^AgreaterB")
-        } 
-        else if(iconLink.includes("^A>=B")) {
-          newIconLink = iconLink.replace("^A>=B", "^Agreater=B")
-        }  
-        else if(iconLink.includes("/AmodR2")) {
-          newIconLink = iconLink.replace("/AmodR2", ":AmodR2")
-        }  
-        else if(iconLink.includes("/BToAmodR2")) {
-          newIconLink = iconLink.replace("/BToAmodR2", ":BToAmodR2")
-        }  
-        else {
-          newIconLink = iconLink
-        }
+        console.log("object", gate);
+        const gateView = document.createElement("span");
+        gateView.setAttribute("class", "list-view tooltip-wrap");
         gateView.innerHTML = `
-                      <img onmousedown="grabGate('${gate.serializedId}')" src=${newIconLink} style="width:36px; height:36px; margin: 5px 6px;"/>
-                      <span class="tooltip-content">${gate.name}</span>
+                  <div style='display: flex; align-items:center; justify-content:center; width:40px; height:40px; margin:0.3rem 0.25rem; border: 1px solid black'>
+                    <div style='text-align:center;'>
+                      ${newIcon.get(gate.symbol) != null? newIcon.get(gate.symbol) : gate.symbol}
+                    </div>
+                  </div>
+                  <span class="tooltip-content">${gate.name}</span>
                 `;
-        advancedGates.appendChild(gateView)
+        advancedGates.appendChild(gateView);
       }
     });
   })
@@ -197,7 +233,7 @@ const initGateViews = () => {
     let gateGroup = document.createElement('div'); gateGroup.setAttribute("class", "gate-group")
     let gateGroupName = document.createElement('h4'); gateGroupName.setAttribute("class", `group-gate-name`);
     gateGroupName.innerText = group.hint
-    let createButton = document.createElement("button"); createButton.setAttribute("class",`toggle-gate-list ${"group-gate-num" + groupNum}`); createButton.setAttribute("data-state", "open");
+    let createButton = document.createElement("button"); createButton.setAttribute("class", `toggle-gate-list ${"group-gate-num" + groupNum}`); createButton.setAttribute("data-state", "open");
     createButton.innerHTML = `
     <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 32 32" class="AccordionTriggerIcon"><path d="M16 22L6 12 7.4 10.6 16 19.2 24.6 10.6 26 12z"></path></svg>
   `;
@@ -213,30 +249,30 @@ const initGateViews = () => {
         gateView.setAttribute("class", "grid-view")
         const iconLink = `./icon/${gate.serializedId}.png`
         let newIconLink = ''
-        if(iconLink.includes(">>3")) {
+        if (iconLink.includes(">>3")) {
           newIconLink = iconLink.replace(">>3", "2greater")
-        } 
-        else if(iconLink.includes("(/)")) {
+        }
+        else if (iconLink.includes("(/)")) {
           newIconLink = iconLink.replace("(/)", "(:)")
-        } 
-        else if(iconLink.includes("|/⟩⟨/|")) {
+        }
+        else if (iconLink.includes("|/⟩⟨/|")) {
           newIconLink = iconLink.replace("|/⟩⟨/|", "|:⟩⟨:|")
         }
-        else if(iconLink.includes("/A2")) {
+        else if (iconLink.includes("/A2")) {
           newIconLink = iconLink.replace("/A2", ":A2")
-        }  
-        else if(iconLink.includes("^A>B")) {
+        }
+        else if (iconLink.includes("^A>B")) {
           newIconLink = iconLink.replace("^A>B", "^AgreaterB")
-        } 
-        else if(iconLink.includes("^A>=B")) {
+        }
+        else if (iconLink.includes("^A>=B")) {
           newIconLink = iconLink.replace("^A>=B", "^Agreater=B")
-        }  
-        else if(iconLink.includes("/AmodR2")) {
+        }
+        else if (iconLink.includes("/AmodR2")) {
           newIconLink = iconLink.replace("/AmodR2", ":AmodR2")
-        }  
-        else if(iconLink.includes("/BToAmodR2")) {
+        }
+        else if (iconLink.includes("/BToAmodR2")) {
           newIconLink = iconLink.replace("/BToAmodR2", ":BToAmodR2")
-        }  
+        }
         else {
           newIconLink = iconLink
         }
@@ -253,4 +289,4 @@ const initGateViews = () => {
 
 }
 
-export {initGateViews}
+export { initGateViews }
