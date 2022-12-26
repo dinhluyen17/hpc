4b6d6d201eeedc6dc96144f37cecbbf43345793d
Khoi <khoinl@mz.co.kr>
2022-12-26 16:57:24 +0700

    fix anti control icon position

--- quantum-composer/src/ui/initGateViews.js
+++ quantum-composer/src/ui/initGateViews.js
@@ -58,7 +58,7 @@ const initGateViews = () => {
   //new custom gate symbol
   const newIcon = new Map([
     ['Measure', '<small class="smaller">Measure</small>'],
-    ['◦', '<big class="bigger">○</big>'],
+    ['◦', '<big class="bigger" style="padding-left: 3px;">○</big>'],
     ['•', '<big class="bigger">●</big>'],
     ['Density', '<small class="smaller">Density</small>'],
     ['Bloch', '<small>Bloch</small>'],
