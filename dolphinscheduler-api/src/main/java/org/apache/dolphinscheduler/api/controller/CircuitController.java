0e1a3cfce771ebcaa843a5ee5f8ed65bcc8aaf60
dinhluyen17 <luyendd@mz.co.kr>
2023-01-30 10:21:22 +0700

    Io library

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/CircuitController.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/CircuitController.java
@@ -36,15 +36,14 @@ import org.apache.dolphinscheduler.plugin.task.api.utils.ParameterUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
