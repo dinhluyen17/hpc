c4da922e31c0805f61670506db2272a4a36fb8fb
dinhluyen17 <luyendd@mz.co.kr>
2023-02-02 11:28:10 +0700

    revert fix ctx.roundRect is not a function

--- quantum-composer/src/draw/Painter.js
+++ quantum-composer/src/draw/Painter.js
@@ -155,12 +155,7 @@ class Painter {
      */
     fillRect(rect, color = Config.DEFAULT_FILL_COLOR) {
         this.ctx.beginPath();
-        try {
-            this.ctx.roundRect(rect.x, rect.y, rect.w, rect.h,[5]); //for google chrome users
-        }
-         catch(error) {
-            this.ctx.fillRect(rect.x, rect.y, rect.w, rect.h,[5]); //fall back to quare shape gate for firefox
-        }
+        this.ctx.roundRect(rect.x, rect.y, rect.w, rect.h,[5]);
         this.ctx.fillStyle = color;
         this.ctx.fill();
     }
